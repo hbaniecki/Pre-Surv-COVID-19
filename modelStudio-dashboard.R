d6277d79fc0a63c0579af0b06c2f3dd614eb70a1
hbaniecki <hbaniecki@gmail.com>
2020-10-21 18:18:17 +0200

    dss

--- modelStudio-dashboard.R
+++ modelStudio-dashboard.R
@@ -2,13 +2,14 @@
 library(reticulate)
 explainer <- py_load_object("xgboost-explainer.pkl", pickle = "pickle")
 
+# pick observations and adjust their labels
 set.seed(0)
-local_obs <- explainer$data[sample(1:nrow(explainer$data), 15),]
+local_obs <- explainer$data[sample(1:nrow(explainer$data), 5), ]
 rownames(local_obs) <- paste("id:", rownames(local_obs),
                              "| pred:", 
                              round(explainer$predict(local_obs), 4))
 
 # make a studio for the model
 library(modelStudio)
-ms <- modelStudio(explainer, local_obs, B = 20)
+ms <- modelStudio(explainer, local_obs, B = 15)
 ms
